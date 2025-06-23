91ca55c4944b4291a1eed6d3e3662dd5729071d9
ayushmittal1003 <ayush@goodeducator.com>
2025-06-23 22:12:35 +0530

    Hero banner bottom line removal

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -140,7 +140,15 @@ const Hero: React.FC = () => {
       </div>
       
       {/* Wave shape divider */}
-     
+     <div className="absolute bottom-0 left-0 right-0">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 100" className="w-full h-auto">
+    <path 
+      fill="#ffffff" 
+      fillOpacity="1" 
+      d="M0,96L48,85.3C96,75,192,53,288,53.3C384,53,480,75,576,90.7C672,107,768,117,864,112C960,107,1056,85,1152,74.7C1248,64,1344,64,1392,64L1440,64"
+    />
+  </svg>
+</div>
     </section>
   );
 };
