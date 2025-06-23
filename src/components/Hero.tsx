865612fe5d2b09fdf4cbd70973b67bae43b665d1
ayushmittal1003 <ayush@goodeducator.com>
2025-06-23 22:08:53 +0530

    Remove bottom line from Hero Banner
    Divider removal

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -140,15 +140,7 @@ const Hero: React.FC = () => {
       </div>
       
       {/* Wave shape divider */}
-      <div className="absolute bottom-0 left-0 right-0">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 120" className="w-full h-auto">
-          <path 
-            fill="#ffffff" 
-            fillOpacity="1" 
-            d="M0,96L48,85.3C96,75,192,53,288,53.3C384,53,480,75,576,90.7C672,107,768,117,864,112C960,107,1056,85,1152,74.7C1248,64,1344,64,1392,64L1440,64L1440,120L1392,120C1344,120,1248,120,1152,120C1056,120,960,120,864,120C768,120,672,120,576,120C480,120,384,120,288,120C192,120,96,120,48,120L0,120Z"
-          />
-        </svg>
-      </div>
+     
     </section>
   );
 };
